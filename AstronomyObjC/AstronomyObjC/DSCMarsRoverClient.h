9a601f157ca551f53d26eeb2dd7054d9f1266220
Denis Cedeno <denis.cedeno@gmail.com>
2020-06-04 16:36:42 -0400

    reconfigured files

---
+++ AstronomyObjC/AstronomyObjC/DSCMarsRoverClient.h
@@ -0,0 +1,17 @@
+//
+//  DSCMarsRoverClient.h
+//  AstronomyObjC
+//
+//  Created by denis cedeno on 6/4/20.
+//  Copyright © 2020 DenCedeno Co. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DSCMarsRoverClient : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AstronomyObjC/AstronomyObjC/DSCSolDescription.m
@@ -0,0 +1,13 @@
+//
+//  DSCSolDescription.m
+//  AstronomyObjC
+//
+//  Created by denis cedeno on 6/4/20.
+//  Copyright © 2020 DenCedeno Co. All rights reserved.
+//
+
+#import "DSCSolDescription.h"
+
+@implementation DSCSolDescription
+
+@end
